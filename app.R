2e6b0d31676edece4e761df404264357ae65634e
Neena05 <68971089+Neena05@users.noreply.github.com>
2025-09-30 10:16:25 -0500

    Add multiply button

--- app.R
+++ app.R
@@ -1,31 +1,42 @@
 library(shiny)
 
 ui <- fluidPage(
-    titlePanel("Let's Do Arithmetic!"),
-    sidebarLayout(
-        sidebarPanel(
-            numericInput(inputId = "number1",
-                         label = "Number 1",
-                         value = 0),
-            numericInput(inputId = "number2",
-                         label = "Number 2",
-                         value = 0)
-        ),
-        mainPanel(
-            h2("The sum of the two numbers is:"),
-            textOutput("sum")
-        )
+  titlePanel("Let's Do Arithmetic!"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "number1",
+                   label = "Number 1",
+                   value = 0),
+      numericInput(inputId = "number2",
+                   label = "Number 2",
+                   value = 0),
+      actionButton("multiply", "Multiply")
+    ),
+    mainPanel(
+      h2("The sum of the two numbers is:"),
+      textOutput("sum"),
+      h2("The product of the two numbers is:"),
+      textOutput("product")
     )
+  )
 )
 
 server <- function(input, output) {
-    output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
-        x1 + x2
+  output$sum <- renderText({
+    x1 <- as.numeric(input$number1)
+    x2 <- as.numeric(input$number2)
+    x1 + x2
+  })
+  
+  output$product <- renderText({
+    input$multiply
+    isolate({
+      x1 <- as.numeric(input$number1)
+      x2 <- as.numeric(input$number2)
+      x1 * x2
     })
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
 
